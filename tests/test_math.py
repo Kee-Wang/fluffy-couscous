20534e8c327b453fe659cb8a11e7f0b7581d1b62
Qingfeng Wang <kee@Qingfengs-MacBook-Pro.local>
2017-07-25 15:37:53 -0400

    all added

--- tests/test_math.py
+++ tests/test_math.py
@@ -3,7 +3,7 @@ Testing for the math.py module
 This is called testing framework.
 Use
 python -c "import pytest"
-before running this script.
+before running this script to check if `pytest` is installed.
 After scripts are done, use
 `py.test -v`
 """
@@ -15,7 +15,13 @@ def test_add():
     assert fc.math.add(5,2) == 7
     assert fc.math.add(2,5) == 7
 
-
-def test_mult():
-    assert fc.math.mult(5,2) == 10
-    assert fc.math.mult(2,5) == 10
+testdata = [
+    (2, 5, 10),
+    (1, 2, 2),
+    (11, 9, 99),
+    (0, 0, 0)
+]
+@pytest.mark.parametrize("a, b, expected", testdata)
+def test_mult(a, b, expected):
+    assert fc.math.mult(a, b) == expected
+    assert fc.math.mult(b, a) == expected
